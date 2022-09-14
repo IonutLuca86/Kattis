e84e111fca61d78401c4f2a6950095afc7a27bfe
Luca Ionut-Catalin <112466341+IonutLuca86@users.noreply.github.com>
2022-09-14 11:16:56 +0200

    Add files via upload
    Kattis Jumbo Javelin problem

---
+++ Jumbo Javelin/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Jumbo Javelin
+//Input
+//The first line consist of an integer N , the number of steel rods she has. Then follows N  lines, each containing a single integer li representing the length of the steel rod in cm.
+
+//Output
+//Print out the length of the jumbo javelin Jack has made for her.
+
+//Limits
+//1<N<=100
+//1<=li<=50
+
+
+int n = Int32.Parse(Console.ReadLine());
+int[] li = new int[n];
+int output = 0;
+for (int i = 0; i < n; i++)
+{
+    li[i] = Int32.Parse(Console.ReadLine());
+    output += li[i];
+}
+output = output - (n - 1);
+Console.WriteLine(output);
